e6693d751064f3e3da2fbf3dcc3ad61514661bd6
Jonathan <johnzballad@gmail.com>
2019-10-29 10:06:44 -0300

    Chamada das funções

--- src/sites/Porvir/index.ts
+++ src/sites/Porvir/index.ts
@@ -2,23 +2,21 @@ const cheerio = require('cheerio');
 import axios from 'axios'
 
 import hasPagination from './hasPagination';
+import calculateNumberOfPages from './calculateNumberOfPages';
 
 export default async function (keyword: String) {
   // 1. Carregar a busca
-  console.time('tempo');
   const { data } = await axios.get(`http://porvir.org/?s=${keyword}&buscar=Enviar`);
-  console.timeEnd('tempo')
   const $ = cheerio.load(data);
 
   // 2. Verificar se há paginação
-
   if (hasPagination($)) {
-    // 2.1 Tem paginação
+    // 2.1 Calcular a quantidade de páginas que possui
+    calculateNumberOfPages($);
     // 2.1 Faz o download de cada item em todas as páginas!
-
   }
-
   // 3. Não há paginação, faço download dos itens dessa página.
+
 
   // 2.1 Havendo paginação, realizar download por cada página
 

8ef1bcadee74e66fda37b3fef364d48da5299288
Jonathan <johnzballad@gmail.com>
2019-11-05 09:15:39 -0300

    corrigindo importações

--- src/sites/Porvir/index.ts
+++ src/sites/Porvir/index.ts
@@ -1,12 +1,12 @@
 import axios, { AxiosResponse } from 'axios'
 import cheerio from 'cheerio';
+import UrlBuilder from '../../tools/UrlBuilder';
 import calculateNumberOfPages from './calculateNumberOfPages';
-import hasPagination from './hasPagination';
 import downloadContent from './downloadContent';
+import hasPagination from './hasPagination';
 
 export default async function (keyword: string) {
-  
-  const url:string = `http://porvir.org/?s=${keyword}&buscar=Enviar`;
+  const url = `http://porvir.org/?s=${keyword}&buscar=Enviar`;
 
   // 1. Carregar a busca
   const response: AxiosResponse = await axios.get(url);
@@ -17,13 +17,13 @@ export default async function (keyword: string) {
   if (hasPagination($)) {
 
     const pages = calculateNumberOfPages($);
-    
+
     // Aplicar o paralelismo aqui ====================
 
-    for (let page = 1; page < pages+1; page++) {    
-      
+    for (let page = 1; page < pages + 1; page++) {
+
       const urlPerPage = UrlBuilder.porVirUrlPerPage(keyword, page);
-      
+
       downloadContent(urlPerPage);
     }
 

795a1bca2b5a6d8c511beaf266cf46988d81ce8f
Jonathan <johnzballad@gmail.com>
2019-11-05 09:14:02 -0300

    consertando url

--- src/tools/UrlBuilder.ts
+++ src/tools/UrlBuilder.ts
@@ -1,7 +1,7 @@
-class UrlBuilder{
-
-    static porVirUrlPerPage (keyword:string, page: number): string {
-        return `http://porvir.org/s=${keyword}&c=&cs=&t=&pg=${page}`;
-    }
-
+class UrlBuilder {
+  public static porVirUrlPerPage(keyword: string, page: number): string {
+    return `http://porvir.org/?s=${keyword}&c=&cs=&t=&pg=${page}`;
+  }
 }
+
+export default UrlBuilder;

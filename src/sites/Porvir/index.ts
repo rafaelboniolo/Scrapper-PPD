4666c98766155d7ba0982e1f94ae56a9cbe470f2
Jonathan <johnzballad@gmail.com>
2019-11-16 04:41:23 -0300

    Usando as novas funções de request/loadcheerio

--- src/sites/Porvir/index.ts
+++ src/sites/Porvir/index.ts
@@ -3,29 +3,33 @@ import parallelDownload from '../../tools/parallelDownload';
 import sequencialDownload from '../../tools/sequencialDownload';
 import UrlBuilder from '../../tools/UrlBuilder';
 import calculateNumberOfPages from './calculateNumberOfPages';
-import downloadContent from './downloadContent';
 
 import debug from 'debug';
+import loadCheerio from '../../tools/loadCheerio';
+import Request from '../../tools/Request';
 const DEBUG = debug("Porvir::Main");
 
 const USE_PARALLELISM = process.env.USE_PARALLELISM || false;
 const PorvirWorkerPath = resolve(__dirname, "worker.ts");
 
 export default async function (keyword: string) {
-  const url = UrlBuilder.porVirUrl(keyword);
+  const URL = UrlBuilder.porVirUrl(keyword);
 
-  // $ is a loaded cheerio instance
-  const $ = await downloadContent(url);
+  const pageHTML = await Request(URL);
+
+  const $ = loadCheerio(pageHTML);
 
   const pages = calculateNumberOfPages($);
 
   if (USE_PARALLELISM) {
     DEBUG("Usando paralelismo!");
     parallelDownload(pages, keyword, PorvirWorkerPath);
-  } else {
-    DEBUG("Usando Sequencial!");
-    sequencialDownload(pages, keyword);
+    return;
   }
+
+  DEBUG("Usando Sequencial!");
+  sequencialDownload(pages, keyword);
+  return;
 }
 
 
